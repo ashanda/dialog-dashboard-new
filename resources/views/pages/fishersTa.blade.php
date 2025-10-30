e60e5d31a3a5158f13862aba479fdaa4f8a429a6
GeekMac <ashandevinda9485@gmail.com>
2025-10-30 11:52:49 +0530

    done of the day

--- resources/views/pages/fishersTa.blade.php
+++ resources/views/pages/fishersTa.blade.php
@@ -15,8 +15,30 @@
                 <h5 class="mb-0 text-white fw-bold" style="font-size: 22px;">{{ $fisher->tamil_name }}</h5>
             </div>
 
-            <!-- Card Image -->
-            <img src="{{ asset('storage/'.$fisher->image) }}" class="card-img-top" alt="Fisher Image">
+            <!-- Card Image Slider -->
+            <div id="fisherCarousel{{ $fisher->id }}" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    @for ($i = 1; $i <= 5; $i++)
+                        @php $field = 'image_day' . $i; @endphp
+                        @if(!empty($fisher->$field))
+                        <div class="carousel-item @if($i == 1) active @endif">
+                            <img src="{{ asset('storage/' . $fisher->$field) }}" class="d-block w-100" alt="Day {{ $i }} Image">
+                            <div class="carousel-caption d-none d-md-block">
+                                <span class="badge bg-primary">Day {{ $i }}</span>
+                            </div>
+                        </div>
+                        @endif
+                    @endfor
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#fisherCarousel{{ $fisher->id }}" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#fisherCarousel{{ $fisher->id }}" data-bs-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
             
             <!-- Card Footer -->
             <div class="card-footer text-center">
